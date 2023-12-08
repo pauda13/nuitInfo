e1d635bace5370af42ccf64c0d4f56ece9bf3da6
Pauline <pauline.auda@free.fr>
2023-12-08 07:24:37 +0100

    sapin

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,15 +1,23 @@
 $(document).ready(function (){
+
     compt = 0;
     $("#sapin").click((e)=>{
         console.log(e);
 
         const elemId = e.target.id;
-        //alert(elemId)
        if(elemId.includes("lampe-")){
            $('#'+elemId).css('background-color','#f8cf00');
-           //alert('test')
-        } else {
+        } else if(!elemId.includes("sapin")) {
             addLamp(e.clientX,e.clientY);
+        }
+    })
+    $("#sapin").ondblclick(function (e) {
+        const elemId = e.target.id
+        if(elemId.includes("lampe-")){
+            var color = $('#'+elemId).css('background-color')
+            if(color === '#f8cf00' ){
+                alert("Vous venez de trouver un tresor")
+            }
         }
     })
 })
